3bd3ccf3a7454bb7f7b1f2e4c0eb92061fca8b7c
plutos28 <gamevictor28@gmail.com>
2022-12-20 18:38:10 +0300

    make cursor switch state on hoverable and remove child visibility

--- main.js
+++ main.js
@@ -1,2 +1,30 @@
 // Create logo text into a circle
 new CircleType(document.getElementById("nav-logo"));
+
+// Create the custom cursor with extra effects
+let kursorx = new kursor({
+    type: 2,
+    color: "#000"
+})
+
+// Change the dimensions of the cursor when it hovers over hoverables
+const hoverables = [...document.querySelectorAll(".hoverable")];
+
+hoverables.forEach(hoverable => {
+    hoverable.addEventListener("mouseenter", onMouseEnterHoverable);
+    hoverable.addEventListener("mouseleave", onMouseLeaveHoverable);
+});
+
+function onMouseEnterHoverable(e) {
+    console.log(`entering ${e.srcElement.id}`);
+
+    // Change the blending mode and size of kursor and kursorChild
+    kursorx.kursor.classList.add("cursor-hover");
+}
+
+function onMouseLeaveHoverable(e) {
+    console.log(`leaving ${e.srcElement.id}`);
+
+    // Change the blending mode and size of kursor and kursorChild
+    kursorx.kursor.classList.remove("cursor-hover");
+}
